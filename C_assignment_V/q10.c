ac348133463f5e66cdf677f3f0a910704b117250
idityage <am44910606@gmail.com>
2024-03-28 18:02:55 +0530

    formated

--- C_assignment_V/q10.c
+++ C_assignment_V/q10.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #define MAX_SIZE 100
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n)
+{
     int i, j, temp;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int main() {
+int main()
+{
     int arr[MAX_SIZE];
     int i, n;
 
@@ -28,11 +33,12 @@ int main() {
     }
 
     bubbleSort(arr, n);
-    
+
     printf("Sorted array in ascending order: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
-    
+
     return 0;
 }
