8e9c3ffe3ad44a8bde2f503a53dd9bc7328ec002
idityage <am44910606@gmail.com>
2024-04-12 04:49:34 +0530

    Add sum of all elements in the matrix to q19.c

--- C_assignment_V/q19.c
+++ C_assignment_V/q19.c
@@ -30,7 +30,16 @@ int main(){
         printf("\n");
     }
     for (int i = 0; i < rows; i++){
-        printf("Sum of row %d: %d\n", i, sumOfRow(matrix, cols, i));
+        printf("Sum of row %d: %d\n", i+1, sumOfRow(matrix, cols, i));
     }
+    // sum of all elements in the matrix
+    int sum = 0;
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            sum += matrix[i][j];
+        }
+    }
+    printf("Sum of all elements in the matrix: %d\n", sum);
+
     return 0;
 }
