a03e8677f9efd54ddbf602d211589a99242343bd
idityaGE <am44910606@gmail.com>
2024-08-04 23:41:25 +0530

    q4 is done

---
+++ cpp/ClassAssignment_2/q4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Polynomial
+{
+  int degree;
+  int *coefficients;
+};
+
+double cal_poly(Polynomial &poly, double x)
+{
+  double result = 0;
+  for (int i = 0; i <= poly.degree; i++)
+  {
+    result += poly.coefficients[i] * pow(x, i);
+  }
+  return result;
+}
+
+int main()
+{
+  Polynomial poly;
+  cout << "Enter the degree of the polynomial: ";
+  cin >> poly.degree;
+
+  poly.coefficients = new int[poly.degree + 1];
+  cout << "Enter the coefficients of the polynomial: " << endl;
+  for (int i = 0; i <= poly.degree; i++)
+  {
+    cout << "Enter the coefficient of x^" << i << " : ";
+    cin >> poly.coefficients[i];
+  }
+
+  double x;
+  cout << "Enter the value of x: ";
+  cin >> x;
+
+  double result = cal_poly(poly, x);
+  cout << "The result of the polynomial equation for x = " << x << " is: " << result << endl;
+
+  delete[] poly.coefficients;
+  return 0;
+}
