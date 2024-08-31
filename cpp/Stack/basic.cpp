3b01dd792e8ea3cfdfb481a5a02bad1d64c8b39e
idityaGE <am44910606@gmail.com>
2024-09-01 00:05:52 +0530

    srtack

---
+++ cpp/Stack/basic.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct Stack {
+ private:
+  int size;
+  int top;
+  int *S;
+
+ public:
+  void create(int s);
+  void push(int x);
+  int pop();
+  int peek(int index);
+  int isFull();
+  int isEmpty();
+  void display();
+};
+
+void Stack::create(int s) {
+  S = new int[s];
+  size = s;
+  top = -1;
+}
+
+void Stack::push(int x) {
+  
+}
+
+int main() {
+  return 0;
+}
